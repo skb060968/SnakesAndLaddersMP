3b233b3185b92c1c1498b47a1b42806a2f2e0613
Sunil <skb060968@gmail.com>
2026-05-21 08:19:18 +0530

    new look

--- public/sw.js
+++ public/sw.js
@@ -4,7 +4,7 @@
    - Cache-first for static assets (images, sounds, icons)
    ============================== */
 
-const CACHE_NAME = "snl-mp-v15";
+const CACHE_NAME = "snl-mp-v17";
 
 const STATIC_ASSETS = [
   "/",
