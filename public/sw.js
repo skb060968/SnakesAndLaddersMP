62134b39e0956fad737521d00a2f707375b27564
Sunil <skb060968@gmail.com>
2026-06-14 09:16:57 +0530

    latest update

--- public/sw.js
+++ public/sw.js
@@ -4,7 +4,7 @@
    - Cache-first for static assets (images, sounds, icons)
    ============================== */
 
-const CACHE_NAME = "snl-mp-v13";
+const CACHE_NAME = "snl-mp-v14";
 
 const STATIC_ASSETS = [
   "/",
