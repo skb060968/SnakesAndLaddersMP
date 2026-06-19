aa08ec7700f7779ffa31e801790becba110aaf62
Sunil <skb060968@gmail.com>
2026-06-19 17:10:00 +0530

    new home screen

--- public/sw.js
+++ public/sw.js
@@ -4,7 +4,7 @@
    - Cache-first for static assets (images, sounds, icons)
    ============================== */
 
-const CACHE_NAME = "snl-mp-v24";
+const CACHE_NAME = "snl-mp-v25";
 
 const STATIC_ASSETS = [
   "/",
